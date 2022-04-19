9fc2545e4fe4d1889fc4a915b12dd9de8b53e86c
Xavier Leprêtre <xavier.lepretre@gmail.com>
2022-04-19 21:41:49 +0400

    Add Checkers Signing Stargate extensions to test and play a game.

--- src/types/checkers/extensions-gui.ts
+++ src/types/checkers/extensions-gui.ts
@@ -2,20 +2,27 @@ import Long from "long"
 import { CheckersSigningStargateClient } from "../../checkers_signingstargateclient"
 import { CheckersStargateClient } from "../../checkers_stargateclient"
 import { IGameInfo } from "../../sharedTypes"
+import { QueryCanPlayMoveResponse } from "../generated/checkers/query"
 import { StoredGame } from "../generated/checkers/stored_game"
-import { storedToGameInfo } from "./board"
+import { guiMoveToPos, storedToGameInfo } from "./board"
 import { getCreatedGameId } from "./events"
 
 declare module "../../checkers_stargateclient" {
     interface CheckersStargateClient {
         getGuiGames(): Promise<IGameInfo[]>
         getGuiGame(index: string): Promise<IGameInfo | undefined>
+        canPlayGuiMove(
+            gameIndex: string,
+            playerId: number,
+            move: number[][],
+        ): Promise<QueryCanPlayMoveResponse>
     }
 }
 
 declare module "../../checkers_signingstargateclient" {
     interface CheckersSigningStargateClient {
         createGuiGame(creator: string, black: string, red: string): Promise<string>
+        playGuiMove(creator: string, gameIndex: string, move: number[][]): Promise<void>
     }
 }
 
@@ -36,12 +43,34 @@ CheckersStargateClient.prototype.getGuiGame = async function (index: string): Pr
     return storedToGameInfo(storedGame)
 }
 
+CheckersStargateClient.prototype.canPlayGuiMove = async function (
+    gameIndex: string,
+    playerId: number,
+    move: number[][],
+): Promise<QueryCanPlayMoveResponse> {
+    if (playerId < 1 || 2 < playerId) throw new Error(`Wrong playerId: ${playerId}`)
+    return await this.checkersQueryClient!.checkers.canPlayMove(
+        gameIndex,
+        playerId === 1 ? "b" : "r",
+        guiMoveToPos(move[0]),
+        guiMoveToPos(move[1]),
+    )
+}
+
 CheckersSigningStargateClient.prototype.createGuiGame = async function (
     creator: string,
     black: string,
     red: string,
 ): Promise<string> {
     return getCreatedGameId(await this.createGame(creator, black, red, "stake", Long.ZERO, "auto"), 0)
+}
+
+CheckersSigningStargateClient.prototype.playGuiMove = async function (
+    creator: string,
+    gameIndex: string,
+    move: number[][],
+): Promise<void> {
+    await this.playMove(creator, gameIndex, guiMoveToPos(move[0]), guiMoveToPos(move[1]), "auto")
 }
 
 export {}
