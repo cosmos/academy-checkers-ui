1d6a2e0816adc33b6688a1310a92b1c56a426331
Xavier Leprêtre <xavier.lepretre@gmail.com>
2022-04-19 21:39:46 +0400

    Add Checkers Signing Stargate extension to create a game.

--- src/types/checkers/extensions-gui.ts
+++ src/types/checkers/extensions-gui.ts
@@ -1,13 +1,21 @@
 import Long from "long"
+import { CheckersSigningStargateClient } from "../../checkers_signingstargateclient"
 import { CheckersStargateClient } from "../../checkers_stargateclient"
 import { IGameInfo } from "../../sharedTypes"
 import { StoredGame } from "../generated/checkers/stored_game"
 import { storedToGameInfo } from "./board"
+import { getCreatedGameId } from "./events"
 
 declare module "../../checkers_stargateclient" {
     interface CheckersStargateClient {
         getGuiGames(): Promise<IGameInfo[]>
         getGuiGame(index: string): Promise<IGameInfo | undefined>
+    }
+}
+
+declare module "../../checkers_signingstargateclient" {
+    interface CheckersSigningStargateClient {
+        createGuiGame(creator: string, black: string, red: string): Promise<string>
     }
 }
 
@@ -26,6 +34,14 @@ CheckersStargateClient.prototype.getGuiGame = async function (index: string): Pr
     const storedGame: StoredGame | undefined = await this.checkersQueryClient!.checkers.getStoredGame(index)
     if (!storedGame) return undefined
     return storedToGameInfo(storedGame)
+}
+
+CheckersSigningStargateClient.prototype.createGuiGame = async function (
+    creator: string,
+    black: string,
+    red: string,
+): Promise<string> {
+    return getCreatedGameId(await this.createGame(creator, black, red, "stake", Long.ZERO, "auto"), 0)
 }
 
 export {}
